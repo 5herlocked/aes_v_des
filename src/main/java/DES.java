b2d7dfc40db23fb768db3240fa14a6fd4798a395
swooo <liushanting@gmail.com>
2021-04-10 14:07:56 -0400

    Updated variables

--- src/main/java/DES.java
+++ src/main/java/DES.java
@@ -14,8 +14,8 @@ Outputs a 64-bit plaintext
 1. Split up plaintext input into bytes -- partitionBytes()
 2. Create a byte array keyArray to be used as a key -- genKeyArray()
 3. Create a byte array ivArray to be used as an initialization vector (IV) -- genIvArray()
-4. Create a new SecretKeySpec w/ key array -- genSecretKeySpec();
-5. Create a new IvParameterSpec using IV array -- genIvParameterSpec();
+4. Create a new SecretKeySpec secretKeySpec w/ key array -- genSecretKeySpec();
+5. Create a new IvParameterSpec ivParameterSpec using IV array -- genIvParameterSpec();
 6. Create a new Cipher for DES+CBC with PKCS7 padding -- createCipher()
 7. Init Cipher to encryption mode using key array + iv array
 8. Encrypt w/ help of Cipher API -- encrypt()
@@ -31,8 +31,10 @@ Compress the given 64-bit key into 48-bit keys using a 56-bit table
 public class DES {
 	private final int keyLength; // bits
 	private final int rounds;
-	private SecretKeySpec keyArray;
-	private IvParameterSpec ivArray;
+	private byte[] keyArray;
+	private byte[] ivArray;
+	private SecretKeySpec secretKeySpec;
+	private IvParameterSpec ivParameterSpec;
 	Cipher cipher;
 
 	// constructor that sets the key length + # of rounds
@@ -49,18 +51,18 @@ public class DES {
 	// step 2.
 	public void genKeyArray(){
 		// TODO: auto-generate keyBytes[], or at least change values
-		byte[] keyArray = new byte[]{0x01, 0x23, 0x45, 0x67, (byte) 0x89, (byte) 0xab, (byte) 0xcd, (byte) 0xef};
+		keyArray = new byte[]{0x01, 0x23, 0x45, 0x67, (byte) 0x89, (byte) 0xab, (byte) 0xcd, (byte) 0xef};
 	}
 
 	// step 3.
 	public void genIvArray(){
 		// TODO: auto-generate ivBytes[], or at least change values
-		byte[] ivArray = new byte[]{0x07, 0x06, 0x05, 0x04, 0x03, 0x02, 0x01, 0x00};
+		ivArray = new byte[]{0x07, 0x06, 0x05, 0x04, 0x03, 0x02, 0x01, 0x00};
 	}
 
 	// step 4.
 	public void genSecretKeySpec(){
-
+		keyArray = new SecretKeySpec(keyArray, "DES");
 	}
 
 	// step 5.
